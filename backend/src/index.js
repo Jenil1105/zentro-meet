5962c70f2dc5b5c016338f817bded35b7e5ec949
Jenil1105 <unselfstudy@gmail.com>
2026-03-26 23:15:29 +0530

    Initialize backend with Express, Socket.io, and MongoDB connection

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const {createServer} = require("node:http");
+const {Server} = require("socket.io");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const port = process.env.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log("mongo Connected");
+        server.listen(port, () => {
+            console.log(`server started on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
